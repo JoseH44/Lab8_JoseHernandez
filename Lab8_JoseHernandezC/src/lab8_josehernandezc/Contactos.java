95271da12e474af3a1c4f95ecd0f168e36b15642
Jose <jrdjhc45@gmail.com>
2019-12-06 13:21:21 -0600

    creacion de la clase usuario y del jframe form

---
+++ Lab8_JoseHernandezC/src/lab8_josehernandezc/Contactos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8_josehernandezc;
+
+/**
+ *
+ * @author jrdjh
+ */
+public class Contactos extends Usuario {
+
+    public Contactos(String nombre, int edad, String numero, String correo, String direccion) {
+        super(nombre, edad, numero, correo, direccion);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
---
+++ Lab8_JoseHernandezC/src/lab8_josehernandezc/Usuario.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8_josehernandezc;
+
+/**
+ *
+ * @author jrdjh
+ */
+public class Usuario {
+
+    private String nombre;
+    private int edad;
+    private String numero;
+    private String correo;
+    private String direccion;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, int edad, String numero, String correo, String direccion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.numero = numero;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+}
